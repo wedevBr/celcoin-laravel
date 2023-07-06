492db38eb43b00ec9a70621255264eedbb5f1fd6
Aron Peyroteo <aron@mqx.com.br>
2023-07-06 00:54:26 -0300

    created DynamicQRCreate endpoint

--- src/Clients/CelcoinPIXDynamic.php
+++ src/Clients/CelcoinPIXDynamic.php
@@ -3,11 +3,15 @@
 namespace WeDevBr\Celcoin\Clients;
 
 use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\Validator;
 use WeDevBr\Celcoin\Common\CelcoinBaseApi;
+use WeDevBr\Celcoin\Rules\PIX\DynamicQRCreate as DynamicQRCreateRule;
+use WeDevBr\Celcoin\Types\PIX\DynamicQRCreate;
 
 class CelcoinPIXDynamic extends CelcoinBaseApi
 {
     const GET_DYNAMIC_BASE64_ENDPOINT = '/pix/v1/brcode/dynamic/%d/base64';
+    const CREATE_DYNAMIC_QRCODE_ENDPOINT = '/pix/v1/brcode/dynamic';
 
     /**
      * @param int $transactionId
@@ -21,4 +25,14 @@ class CelcoinPIXDynamic extends CelcoinBaseApi
         );
     }
 
+    /**
+     * @param DynamicQRCreate $data
+     * @return array|null
+     * @throws RequestException
+     */
+    final public function createDynamicQRCode(DynamicQRCreate $data): ?array
+    {
+        $body = Validator::validate($data->toArray(), DynamicQRCreateRule::rules());
+        return $this->post(self::CREATE_DYNAMIC_QRCODE_ENDPOINT, $body);
+    }
 }
