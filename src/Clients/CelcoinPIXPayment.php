710e31d6a099b005d7080ddf93a8db768dee4720
Aron Peyroteo <aron@mqx.com.br>
2023-07-17 00:35:59 -0300

    created statusPayment endpoint

--- src/Clients/CelcoinPIXPayment.php
+++ src/Clients/CelcoinPIXPayment.php
@@ -7,13 +7,16 @@ use Illuminate\Support\Facades\Validator;
 use WeDevBr\Celcoin\Common\CelcoinBaseApi;
 use WeDevBr\Celcoin\Rules\PIX\PaymentEmvRules;
 use WeDevBr\Celcoin\Rules\PIX\PaymentEndToEndRules;
+use WeDevBr\Celcoin\Rules\PIX\PaymentStatusRules;
 use WeDevBr\Celcoin\Types\PIX\PaymentEmv;
 use WeDevBr\Celcoin\Types\PIX\PaymentEndToEnd;
+use WeDevBr\Celcoin\Types\PIX\PaymentStatus;
 
 class CelcoinPIXPayment extends CelcoinBaseApi
 {
     const END_TO_END_PAYMENT_ENDPOINT = '/pix/v1/payment/endToEnd';
     const EMV_PAYMENT_ENDPOINT = '/pix/v1/emv';
+    const STATUS_PAYMENT_ENDPOINT = '/pix/v1/payment/pi/status';
 
     /**
      * @param PaymentEndToEnd $params
@@ -38,6 +41,21 @@ class CelcoinPIXPayment extends CelcoinBaseApi
         return $this->post(
             self::EMV_PAYMENT_ENDPOINT,
             $body
+        );
+    }
+
+    /**
+     * @throws RequestException
+     */
+    final public function statusPayment(PaymentStatus $params): ?array
+    {
+        $params = Validator::validate($params->toArray(), PaymentStatusRules::rules());
+        return $this->get(
+            sprintf(
+                '%s?%s',
+                self::STATUS_PAYMENT_ENDPOINT,
+                http_build_query($params)
+            )
         );
     }
 }
---
+++ src/Types/PIX/PaymentStatus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WeDevBr\Celcoin\Types\PIX;
+
+use WeDevBr\Celcoin\Types\Data;
+
+class PaymentStatus extends Data
+{
+    public string $transactionId;
+    public string $endtoendId;
+    public string $clientCode;
+
+    /**
+     * @param array $data
+     */
+    public function __construct(array $data = [])
+    {
+        parent::__construct($data);
+    }
+}
