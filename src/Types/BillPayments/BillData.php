6bc947a9f966b13ac6238ee744e84d5b718f6205
Rinaldo Peligrineli <rinaldo.peligrineli@gmail.com>
2023-10-05 17:32:01 -0300

    change type of attributes BillData

--- src/Types/BillPayments/BillData.php
+++ src/Types/BillPayments/BillData.php
@@ -6,10 +6,10 @@ use WeDevBr\Celcoin\Types\Data;
 
 class BillData extends Data
 {
-    public int $value;
-    public int $originalValue;
-    public int $valueWithDiscount;
-    public int $valueWithAdditional;
+    public float $value;
+    public float $originalValue;
+    public float $valueWithDiscount;
+    public float $valueWithAdditional;
 
     public function __construct(array $data = [])
     {
