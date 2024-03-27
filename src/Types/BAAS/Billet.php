09c9af471012fc9accc23b33b3cdb6eae73c372e
Adeildo Amorim <contato@adeildojunior.xyz>
2024-03-27 16:29:48 -0300

    [Refactor] Refactoring billet data

--- src/Types/BAAS/Billet.php
+++ src/Types/BAAS/Billet.php
@@ -7,11 +7,9 @@ use WeDevBr\Celcoin\Types\Data;
 class Billet extends Data
 {
     public string $externalId;
-    public ?string $merchantCategoryCode;
     public int $expirationAfterPayment;
     public string $dueDate;
     public float $amount;
-    public ?string $key;
     /**
      * @var BilletDebtor
      */
